2f1f2ca0fe96ac59aa2d89ed6d874f187e80dd20
mohsine7 <mohsineaitaabbou@gmail.com>
2023-09-12 17:57:02 -0700

    fivo

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,19 +8,21 @@
 	 * 0 and prints 0 if n is zero
 	 * -1 and prints - if n is less than zero
 	 */
-	int print_sign(int n)
+int print_sign(int n)
+{
+	if (n > 0)
 	{
-		if (n > 0)
-		{
-			_putchar('+');
-			return (1);
-		} else if (n == 0)
-		{
-			_putchar(48);
-			return (0);
-		} else if (n < 0)
-		{
-			_putchar('-');
-		}
-			return (-1);
+		_putchar('+');
+		return (1);
 	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
