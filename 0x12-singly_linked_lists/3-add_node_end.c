e28d33dbf14a069c3c8ad1ae7ba71d5e7b605857
mohsine7 <mohsineaitaabbou@gmail.com>
2023-10-19 18:56:56 -0700

    free list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -4,7 +4,7 @@
  * _strlen - returns the length of a string
  * @s: string to evaluate
  *
- * Return: the length of the string 
+ * Return: the length of the string
  */
 int _strlen(const char *s)
 {
