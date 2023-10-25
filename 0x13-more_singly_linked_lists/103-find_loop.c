f2efdeb2cfc92ee7fc0a3b5dffc87a82fb8adc9a
mohsine7 <mohsineaitaabbou@gmail.com>
2023-10-24 19:00:04 -0700

    correction

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,34 +1,36 @@
 #include "lists.h"
 
 /**
- * find_listint_loop - finds the loop in a linked list
- * @head: linked list to search for
+ * find_listint_loop - Finds the loop in a linked list (if it exists).
+ * @head: A pointer to the head of the linked list.
  *
- * Return: address of the node where the loop starts, or NULL
+ * Return: The address of the node where the loop starts, or NULL if there is no loop.
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *slow = head;
-	listint_t *fast = head;
+    listint_t *slow_ptr = head;
+    listint_t *fast_ptr = head;
 
-	if (!head)
-		return (NULL);
+    if (head == NULL)
+        return NULL;
 
-	while (slow && fast && fast->next)
-	{
-		fast = fast->next->next;
-		slow = slow->next;
-		if (fast == slow)
-		{
-			slow = head;
-			while (slow != fast)
-			{
-				slow = slow->next;
-				fast = fast->next;
-			}
-			return (fast);
-		}
-	}
+    while (fast_ptr != NULL && fast_ptr->next != NULL)
+    {
+        slow_ptr = slow_ptr->next;       // Move the slow pointer one step.
+        fast_ptr = fast_ptr->next->next; // Move the fast pointer two steps.
 
-	return (NULL);
+        // If there's a loop, the slow and fast pointers will eventually meet.
+        if (slow_ptr == fast_ptr)
+        {
+            slow_ptr = head; // Reset the slow pointer to the head.
+            while (slow_ptr != fast_ptr)
+            {
+                slow_ptr = slow_ptr->next;
+                fast_ptr = fast_ptr->next;
+            }
+            return fast_ptr; // Return the starting node of the loop.
+        }
+    }
+
+    return NULL;
 }
